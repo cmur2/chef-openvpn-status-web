420e08f56876083e51ff2819d57ce9fe99312f41
cn <cn@mycrobase.de>
2013-10-07 22:01:54 +0200

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,8 @@ name             "openvpn-status-web"
 maintainer       "Christian Nicolai"
 maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
-description      ""
+description      "Installs openvpn-status-web as gem and configures it"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
-#depends ""
-
-#suggests ""
-
-#supports ""
+supports "debian"
